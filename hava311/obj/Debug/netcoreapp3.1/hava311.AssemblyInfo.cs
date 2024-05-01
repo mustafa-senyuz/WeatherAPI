307306451921f0e2d93d9827ce21b138565b0532
Felix L <mustafa_jenkinksproject@outlook.com>
2024-05-01 16:03:27 +0200

    some files were modified

--- hava311/obj/Debug/netcoreapp3.1/hava311.AssemblyInfo.cs
+++ hava311/obj/Debug/netcoreapp3.1/hava311.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hava311")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e025cbca77081f9abee7e21d43dff0ea78529e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("hava311")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hava311")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
