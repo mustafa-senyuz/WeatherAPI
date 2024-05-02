16f79b35df1854c39fb31f244237a6b3b70f4092
Felix L <mustafa_jenkinksproject@outlook.com>
2024-05-02 11:26:29 +0200

    src file

--- hava311/obj/Debug/netcoreapp3.1/hava311.AssemblyInfo.cs
+++ hava311/obj/Debug/netcoreapp3.1/hava311.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hava311")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df970f7b9060efef9ec80da290d57489a84d80a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65343fcf8d526a876945a9a5e5602bdddea0f28a")]
 [assembly: System.Reflection.AssemblyProductAttribute("hava311")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hava311")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
